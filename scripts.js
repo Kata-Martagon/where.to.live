c3f19b6604e6ad29ce640228bd30e401505bb2cd
Kara de la Marck <karadelamarck@gmail.com>
2016-06-06 16:35:48 +0100

    Made Welcome page; add score functionality; add results page

--- scripts.js
+++ scripts.js
@@ -1,9 +1,31 @@
-var current = 1;
-document.getElementById('Question1').style.display='block';
+var current = 0;
+var score = 0;
+var max = 3;
+
+function startQuestionnaire() {
+  document.getElementById('FirstPage').style.display='none';
+  getNext();
+}
 
 function getNext() {
   var last = current;
   current += 1;
-  document.getElementById('Question' + last).style.display='none';
-  document.getElementById('Question' + current).style.display='block';
+  if (last > 0) {
+    score += getSelected(last);
+    document.getElementById('Question' + last).style.display='none';
+  }
+
+  if (current <= max) {
+    document.getElementById('Question' + current).style.display='block';
+  } else {
+    document.getElementById('Result').style.display='block';
+    document.getElementById('Score').textContent=score;
+  }
+
+  console.log(score);
+}
+
+function getSelected(q){
+  var radio = document.getElementById('MyForm').elements['A' + q];
+  return +radio.value;
 }
